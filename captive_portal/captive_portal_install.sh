6c8d5b91dd05bbc136c5084e160ea14070210ab2
teunvanderploeg <54798753+teunvanderploeg@users.noreply.github.com>
2022-12-20 13:02:19 +0100

    Update the installation script
    Update the installation script

--- captive_portal/captive_portal_install.sh
+++ captive_portal/captive_portal_install.sh
@@ -1,19 +1,22 @@
-# Install apache2, wsgi, pip3 and git
-sudo apt-get install apache2 libapache2-mod-wsgi-py3 python3-pip git -y
+# Install apache2, wsgi, pip3 mysql-server and git
+sudo apt-get install apache2 libapache2-mod-wsgi-py3 python3-pip git mysql-server python3-virtualenv libmysqlclient-dev -y
 # enable apache2 module wsgi
 sudo a2enmod wsgi
 # Set Python3 as default Python
 sudo ln -sf /usr/bin/python3 /usr/bin/python
 
+# Start mysql server
+sudo systemctl start mysql.service
+
 # Get git corendon-captive-portal repository and move to correct directory
 sudo git clone https://github.com/teunvanderploeg/corendon-captive-portal.git /var/www/html/corendon-captive-portal
 
 # Create virtual environment
-sudo virtualenv /var/www/html/corendon-captive-portal
+sudo virtualenv /var/www/html/corendon-captive-portal venv
 # Activate virtual environment
-sudo . /var/www/html/corendon-captive-portal/venv/bin/activate
+sudo ./var/www/html/corendon-captive-portal/venv/bin/activate
 # Installing flask module in venv
-sudo /var/www/html/corendon-captive-portal/venv/bin/pip install Flask
+sudo /var/www/html/corendon-captive-portal/venv/bin/pip3 install -r /var/www/html/corendon-captive-portal/requirements.txt
 
 # Apache2 config for wsgi and flask site
 sudo cat >> /etc/apache2/sites-available/captive-portal.conf << EOF
@@ -42,6 +45,9 @@ import sys
 sys.path.insert(0,"/var/www/html/captive-portal")
 from app import app as application
 EOF
+
+# Setup database
+sudo mysql < /var/www/html/corendon-captive-portal/database_setup.sql
 
 # Reload apache2 to load the right config
 sudo systemctl reload apache2
