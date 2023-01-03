f2717df0176d5cde11a41f2a5f38122f259045ce
teunvanderploeg <29253@ma-web.nl>
2023-01-03 19:56:57 +0100

    Install all packages

---
+++ apt/apt_install.sh
@@ -0,0 +1,1 @@
+sudo apt-get install dnsmasq hostapd rfkill dhcpcd5 iptables apache2 libapache2-mod-wsgi-py3 python3-pip git mysql-server python3-virtualenv libmysqlclient-dev nodejs npm ipset iptables netfilter-persistent ipset-persistent iptables-persistent -y
