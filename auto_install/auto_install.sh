cbd41216c7a476510b38b1bd43dc30fab9085221
teunvanderploeg <29253@ma-web.nl>
2023-01-05 11:16:54 +0100

    Made auto install script work

--- auto_install/auto_install.sh
+++ auto_install/auto_install.sh
@@ -1,8 +1,8 @@
-sudo chmod +X ./access_point/access_point_install.sh
-sudo chmod +X ./access_point/access_point_start.bash
-sudo chmod +X ./captive_portal/captive_portal_install.sh
-sudo chmod +X ./firewall/firewall_install.bash
+sudo chmod +x ./../access_point/access_point_install.sh
+sudo chmod +x ./../access_point/access_point_start.bash
+sudo chmod +x ./../captive_portal/captive_portal_install.sh
+sudo chmod +x ./../firewall/firewall_install.bash
 
-sudo ./access_point/access_point_install.sh
-sudo ./captive_portal/captive_portal_install.sh
-sudo ./firewall/firewall_install.bash
+sudo ./../access_point/access_point_install.sh
+sudo ./../captive_portal/captive_portal_install.sh
+sudo ./../firewall/firewall_install.bash
